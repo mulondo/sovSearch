edadb99eb845dff7206bbab00b17185ecfc803ac
mulondo <mulondo26@gmail.com>
2022-04-09 03:37:13 +0300

    create display message for not none existing films

--- src/pages/Films.tsx
+++ src/pages/Films.tsx
@@ -1,19 +1,36 @@
-import React, { useContext, useRef, RefObject } from 'react'
+import React, { useContext, useRef, useEffect, useState } from 'react'
 import FilmsContext from '../context/FilmsContext'
 import styled from 'styled-components'
 import Card from '../components/Card'
 import axios from "axios";
 
 function Films() {
-  const { films, filteredFilms, setFilteredFilms } = useContext(FilmsContext) as any
+  const { films} = useContext(FilmsContext) as any
+  const [filteredFilms, setFilteredFilms] = useState<string[]>([])
+  const [isFilmFound, setIsFilmFound] = useState<boolean>(true)
   const searchInput = useRef<HTMLInputElement>(null)
-  const handleOnchange = () => {
-    let searchText = searchInput.current?.value
-    axios.get(`https://swapi.dev/api/films/?search=${searchText}`).then(function (response) {
-      setFilteredFilms(response.data.results)
-    })
-    console.log(filteredFilms)
+
+  function debounce(cb:any, delay = 1000) {
+    let timeout:any
+    return (...args:any) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        cb(...args)
+      }, delay)
+    }
   }
+
+  const handleOnchange = debounce(()=>{
+      let searchText = searchInput.current?.value
+      axios.get(`https://swapi.dev/api/films/?search=${searchText}`).then(function (response) {
+        const isFilm: boolean = searchText && response.data.results.length === 0? false: true
+        setIsFilmFound(isFilm)
+        setFilteredFilms(response.data.results)
+      })
+  })
+
+  let newFirms: [] = filteredFilms.length === 0? films: filteredFilms
+
   const Button = styled.button`
     background: ${(props: any) => (props.primary ? 'palevioletred' : 'white')};
     color: ${(props: any) => (props.primary ? 'white' : 'palevioletred')};
@@ -38,8 +55,11 @@ function Films() {
         <input type='text' ref={searchInput} placeholder='Search' onChange={handleOnchange}/>
         <Button>Search</Button>
         <Button>History</Button>
+        <div>
+          {isFilmFound? '': 'The Film is not Found'}
+        </div>
         <Films>
-          {films.map((film: any, i: number) => <Card key={film.episode_id} film={film} />)}
+          {newFirms.map((film: any, i: number) => <Card key={film.episode_id} film={film} />)}
         </Films>
       </div>
     </div>
