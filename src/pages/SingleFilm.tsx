88f37b8c8f487103494879a114bcf4be940a4cec
mulondo <mulondo26@gmail.com>
2022-04-11 01:27:28 +0300

    store searched films

--- src/pages/SingleFilm.tsx
+++ src/pages/SingleFilm.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { useParams } from 'react-router-dom';
+import FilmsContext from "../context/FilmsContext";
 
 export default function SingleFilm() {
- return (
-  <div>
-   My card
-  </div>
- )
+    const { films} = useContext(FilmsContext) as any
+    const params = useParams();
+    const selectedParams = params.episode_id? parseInt(params.episode_id): 0
+    const selectedFilm = films.filter((film:any) => film.episode_id ===  selectedParams)
+    const [film] = selectedFilm
+    const viewedFilms: any = []
+    if(localStorage.getItem("searchedFilms") === null){
+        viewedFilms.push(film)
+        localStorage.setItem("searchedFilms", JSON.stringify(viewedFilms))
+    } else{
+        const storedHistory = JSON.parse(localStorage.getItem("searchedFilms") || '[]')
+        storedHistory.push(film)
+        localStorage.setItem("searchedFilms", JSON.stringify(storedHistory))
+    }
+     return (
+      <div>
+       My card
+         <p>{film.title}</p>
+      </div>
+     )
 }
